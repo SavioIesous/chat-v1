fbdc826394a5dcec1091c3374d26a04efa8d5268
Sávio <rodriguessavio68@gmail.com>
2025-03-10 18:09:09 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -82,7 +82,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080");
+    websocket = new WebSocket("wss://chat-v1-tbqt.onrender.com");
     websocket.onmessage = processMessage
 
     console.log(user)
